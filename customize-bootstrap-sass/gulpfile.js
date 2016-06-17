140da2152e101b1e9b95ecc002684d25c8b01a02
hafabenny <hafabenny@yahoo.com>
2016-06-16 22:33:32 -0700

    updates

--- customize-bootstrap-sass/gulpfile.js
+++ customize-bootstrap-sass/gulpfile.js
@@ -10,7 +10,7 @@ var
     
 // Bootstrap scss source
 var bootstrapSass = {
-        in: './node_modules/bootstrap-sass/'
+        in: '../../../node_modules/bootstrap-sass/'
     };
 
 // Bootstrap fonts source
@@ -31,3 +31,26 @@ var scss = {
         includePaths: [bootstrapSass.in + 'assets/stylesheets']
     }
 };
+
+// copy bootstrap required fonts to dest
+gulp.task('fonts', function () {
+    return gulp
+        .src(fonts.in)
+        .pipe(gulp.dest(fonts.out));
+});
+
+
+// compile scss
+gulp.task('sass', ['fonts'], function () {
+    return gulp.src(scss.in)
+        .pipe(sass(scss.sassOpts))
+        .pipe(gulp.dest(scss.out));
+});
+
+
+// default task
+gulp.task('default', ['sass'], function () {
+     gulp.watch(scss.watch, ['sass']);
+});
+
+
